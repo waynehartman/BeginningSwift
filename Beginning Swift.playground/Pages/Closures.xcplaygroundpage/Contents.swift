36d44fdf3518677bd7ac3de0085b965f26c7d364
whartman <wayne.hartman@usaa.com>
2016-08-12 14:23:13 -0500

    Filled out the Functions and Closures sections

--- Beginning Swift.playground/Pages/Closures.xcplaygroundpage/Contents.swift
+++ Beginning Swift.playground/Pages/Closures.xcplaygroundpage/Contents.swift
@@ -1,35 +1,45 @@
 import Foundation
 
-/*  Super useful reference for closure syntax:
+/*  
+ *  Super useful reference for closure syntax:
  *  http://goshdarnclosuresyntax.com/
  */
 
 /*  Variable  */
 
+let variableClosure = {(left: Int, right: Int) -> Int in
+    return left + right
+}
 
-
+variableClosure(5, 5)
 
 
 /*  Optional  */
 
+var optionalClosure: ((Void) -> (Void))? = nil
 
+if let unwrappedClosure = optionalClosure {
+    unwrappedClosure()
+}
 
 
 
 /*  typealias  */
 
-
-
+typealias AliasedClosure = (String) -> (Void)
 
 
 /*  Function Argument  */
 
-
-
+func doSomething(_ closure: AliasedClosure) {
+    closure("Hello!")
+}
 
 
 /*  Function Parameter  */
 
-
+doSomething({ (value: String) -> (Void) in
+    print("\(value), strange sir.")
+})
 
 
--- Beginning Swift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
+++ Beginning Swift.playground/Pages/Functions.xcplaygroundpage/Contents.swift
@@ -3,35 +3,104 @@ import Foundation
 
 /*  Declaring Functions */
 
+func doWork() {
+    print("doing some work")
+}
 
+func doMoreWork() -> String {
+    return "did the needful"
+}
 
+func doMoreWork(job: String, time: Int) -> String {
+    return "did \(job) for \(time) seconds"
+}
 
 
 /*  Calling Functions  */
 
+doWork()
 
+let result = doMoreWork()
+print(result)
 
+doMoreWork(job: "sweeping", time: 5)
+
+let moreResult = doMoreWork(job: "scrubbing", time: 10)
+print(result)
 
 
 /*  Function argument names vs. labels  */
 
+func scrub(for time: Int) {
+    print("I hate scrubbing for \(time) minutes")
+}
 
+scrub(for:5)
 
 
 
 /*  Tuples  */
 
+func calculateStats(numbers:[Int]) -> (min: Int, max: Int, avg: Int) {
+    var min = 0;
+    var max = 0;
+    var total = 0;
+    
+    for number in numbers {
+        if number < min {
+            min = number
+        }
+        
+        if number > max {
+            max = number
+        }
 
+        total += number
+    }
 
+    let avg = total / numbers.count
+
+    return (min, max, avg)
+}
+
+let stats = calculateStats(numbers: [1, 10, -9, 100])
+
+print("min: \(stats.min)")
+print("max: \(stats.max)")
+print("avg: \(stats.avg)")
 
 
 /*  Variadic Parameters  */
 
+func calculateAverage(numbers: Double...) -> Double {
+    var total: Double = 0
 
+    for number in numbers {
+        total += number
+    }
 
+    return total / Double(numbers.count)
+}
 
 
 /*  In-Out Parameters  */
+
+var names = ["Wayne", "Aaron", "Seth"]
+
+var copy = names
+copy.removeLast()
+
+print(names)
+print(copy)
+
+func removeByRef( items: inout [String]) {
+    items.removeFirst()
+}
+
+removeByRef(items: &names)
+
+print(names)
+
 
 
 
@@ -39,18 +108,42 @@ import Foundation
 
 /*  Functions as types  */
 
+func add(left: Int, right: Int) -> Int {
+    return left + right
+}
 
+func subtract(left: Int, right: Int) -> Int {
+    return left - right
+}
+
+var arithmatic = add
+arithmatic(left:5, right:5)
+
+arithmatic = subtract
+arithmatic(left:4, right: 2)
 
 
 
 /*  Functions as parameters  */
 
+func doSomething(function: (Int, Int) -> Int) -> Int {
+    return function(5,5)
+}
 
-
+doSomething(function: add)
+doSomething(function: subtract)
 
 
 /*  Functions as return types  */
 
+typealias IntegerArithmatic = (Int, Int) -> Int
 
+func doTheNeedful() -> IntegerArithmatic {
+    return add
+}
+
+let theNeedful = doTheNeedful()
+
+theNeedful(4, 5)
 
 
