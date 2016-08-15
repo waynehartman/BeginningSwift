b90e2fa4f04791ef1408bc8049d716851da33b89
whartman <wayne.hartman@usaa.com>
2016-08-15 12:19:25 -0500

    Filled in Classes & Structs section

--- Beginning Swift.playground/Pages/Classes & Structs.xcplaygroundpage/Contents.swift
+++ Beginning Swift.playground/Pages/Classes & Structs.xcplaygroundpage/Contents.swift
@@ -3,44 +3,46 @@ import Foundation
 
 /*  Class Definition  */
 
-
-
+class Vehicle {
+    var numberOfWheels: Int?
+    var color: String?
+}
 
 
 /*  Struct Definition  */
 
-
-
-
-
-/*  Stored Properties  */
-
-
-
+struct Point {
+    let x: Int
+    let y: Int
+}
 
 
 /*  Methods  */
 
-
-
-
+struct Person {
+    var firstName: String?
+    var lastName: String?
+    
+    func greet(person: Person) -> String {
+        return "Hello, \(person.firstName)."
+    }
+}
 
 /*  Initializers  */
 
-
-
+class Polygon {
+    let sides: Int
+    
+    init(sides: Int) {
+        self.sides = sides
+    }
+}
 
 
 /*  Creating Instances  */
 
+let car = Vehicle()
+let point = Point(x: 4, y: -9)
+let person = Person(firstName: "Wayne", lastName: "Hartman")
+let square = Polygon(sides: 4)
 
-
-
-
-/*  Methods  */
-
-
-
-
-
-/*  */
