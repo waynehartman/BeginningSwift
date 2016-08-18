b759b8a474feae6283eacc985fb2d20a156b1120
Wayne <waynehartman@gmail.com>
2016-08-18 12:21:12 -0500

    Improved error handling

--- Beginning Swift.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
+++ Beginning Swift.playground/Pages/Error Handling.xcplaygroundpage/Contents.swift
@@ -8,24 +8,34 @@ enum ATMError: Error {
     case maxDailyWithdrawl(maxAmount: Double, remainingAmount: Double)
     case insufficientFunds
     case maxDailyDeposit
+    case sessionInactive
 }
 
 /*  Throwing Errors  */
 
-class ATMTransaction {
+class ATMSession {
     let accountNumber: String
-    var balance: Double
-    var dailyDeposit: Double = 0.0
-    var dailyWithdrawl: Double = 0.0
     let maxDeposit = 3000.0
     let maxWidthdrawl = 1000.0
+    var sessionIsActive = false
+
+    var balance: Double
+
+    var dailyDeposit: Double = 0.0
+    var dailyWithdrawl: Double = 0.0
     
     init(accountNumber: String, balance: Double) {
         self.accountNumber = accountNumber
         self.balance = balance
+        
+        sessionIsActive = true
     }
     
     func withdraw(amount: Double) throws -> (Double, Double) {
+        if sessionIsActive == false {
+            throw ATMError.sessionInactive
+        }
+
         if amount > self.balance {
             throw ATMError.insufficientFunds
         }
@@ -41,13 +51,21 @@ class ATMTransaction {
     }
     
     func deposit(amount: Double) throws -> Double {
+        if sessionIsActive == false {
+            throw ATMError.sessionInactive
+        }
+
         if (amount + self.dailyDeposit) > self.maxDeposit {
             throw ATMError.maxDailyDeposit
         }
-        
+
         self.balance += amount
-        
+
         return self.balance
+    }
+    
+    func endSession() {
+        self.sessionIsActive = false
     }
 }
 
@@ -55,19 +73,35 @@ class ATMTransaction {
 
 /*  Handling Errors  */
 
-var transaction = ATMTransaction(accountNumber: "498438", balance: 2000.0)
+let accountNumber = "34534533"
+
+var transaction = ATMSession(accountNumber: accountNumber, balance: 1000.0)
+//var transaction = ATMSession(accountNumber: accountNumber, balance: 2000.0)
 
 do {
+//    defer {
+//        transaction.endSession()
+//    }
+
     let receipt1 = try transaction.withdraw(amount: 25.00)
     print(receipt1)
+
     let receipt2 = try transaction.withdraw(amount: 900.0)
     print(receipt2)
-    
+
     let receipt3 = try transaction.withdraw(amount: 100.0)
+    print(receipt3)
+
 } catch ATMError.insufficientFunds {
     print("You don't have enough funds.")
 } catch ATMError.maxDailyWithdrawl(let maxAmount, let remainingAmount) {
     print("You may only withdraw \(maxAmount) per day.  You can withdraw \(remainingAmount) more today.")
+}
+
+do {
+    let receipt = try transaction.withdraw(amount: 3454)
+} catch ATMError.sessionInactive {
+    print("Session is inactive")
 }
 
 
