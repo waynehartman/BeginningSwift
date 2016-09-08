bb4b39c44bc1ec863f62daea864ad9cecaf7c17b
whartman <wayne.hartman@usaa.com>
2016-09-08 09:29:10 -0500

    Added advanced collections

---
+++ Beginning Swift.playground/Pages/Advanced Collections.xcplaygroundpage/Contents.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+struct Person {
+    let firstName: String
+    let lastName: String
+    let age: Int
+}
+
+let people = [Person(firstName: "Pedro", lastName: "Manning", age: 35),
+              Person(firstName: "Johnnie", lastName: "Mason", age: 19),
+              Person(firstName: "Wendy", lastName: "Graham", age: 5),
+              Person(firstName: "Lori", lastName: "Cross", age: 89),
+              Person(firstName: "Darla", lastName: "Carpenter", age: 65),
+              Person(firstName: "Emmett", lastName: "Sutton", age: 20),
+              Person(firstName: "Cora", lastName: "Mcdaniel", age: 36),
+              Person(firstName: "Lila", lastName: "Bowman", age: 22),
+              Person(firstName: "Hazel", lastName: "Arnold", age: 10),
+              Person(firstName: "Willie", lastName: "Underwood", age: 41),
+              Person(firstName: "Gustavo", lastName: "Tate", age: 102),
+              Person(firstName: "Stephen", lastName: "Houston", age: 2),
+              Person(firstName: "Angelo", lastName: "Turner", age: 17),
+              Person(firstName: "Erica", lastName: "Bradley", age: 35),
+              Person(firstName: "Janice", lastName: "Wright", age: 29),
+              Person(firstName: "Arnold", lastName: "Clayton", age: 36),
+              Person(firstName: "Alfredo", lastName: "Vaughn", age: 14),
+              Person(firstName: "James", lastName: "Cook", age: 39),
+              Person(firstName: "Christie", lastName: "Chapman", age: 52),
+              Person(firstName: "Karen", lastName: "Torres", age: 69),
+              Person(firstName: "Ramona", lastName: "Gross", age: 90),
+              Person(firstName: "Philip", lastName: "Ford", age: 65),
+              Person(firstName: "Lawrence", lastName: "Arnold", age: 11),
+              Person(firstName: "Ebony", lastName: "Rhodes", age: 30),
+              Person(firstName: "Sandra", lastName: "Quinn", age: 31)]
+
+/*  Map  */
+
+
+
+
+
+/*  Filter  */
+
+
+
+
+
+/*  Reduce  */
+
+
+
+
+
+/*  Sorting  */
+
+
+
+
+
+
+
+/*  EXERCISE:
+ *  Create a list of people to only include people with *odd* ages between 17-35, sort them by last name, first name.
+ *
+ */
+
+
+
+
+
